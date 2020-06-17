95cd44f9315cfc0fc72691eeacff655f0d8d9930
dejanperic <dejan.peric@avssolutions.rs>
2020-06-17 14:48:52 +0200

    cmt 12

--- src/utilities.ts
+++ src/utilities.ts
@@ -1,5 +1,21 @@
+import {browser, ElementFinder, protractor} from 'protractor';
 export class Utilities {
+    
     public sayHelloTo(name: string): string {
       return `🦄 Hello ${name} !`;
     }
+
+    public enterTextInElement(selector: ElementFinder, text: any) {
+        const ec = protractor.ExpectedConditions;
+        browser.wait(ec.visibilityOf(selector));
+        selector.clear().then(() => {
+          selector.sendKeys(text);
+        });
+      }
+    
+      public clickOnElement(selector: ElementFinder): void {
+        const ec = protractor.ExpectedConditions;
+        browser.wait(ec.elementToBeClickable(selector), 5000);
+        selector.click();
+      }
   }
