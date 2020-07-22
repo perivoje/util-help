243cd4dafac1c855a2a2a7113d56f0d0bcce77fc
dejan.peric <dejan.peric@avssolutions.rs>
2020-07-22 15:36:17 +0200

    123

--- src/utilities.ts
+++ src/utilities.ts
@@ -1,21 +1,32 @@
-import {browser, ElementFinder, protractor} from 'protractor';
+import { browser, ElementFinder, protractor } from 'protractor';
 export class Utilities {
-    
-    public sayHelloTo(name: string): string {
-      return `🦄 Hello ${name} !`;
-    }
 
-    public enterTextInElement(selector: ElementFinder, text: any) {
-        const ec = protractor.ExpectedConditions;
-        browser.wait(ec.visibilityOf(selector));
-        selector.clear().then(() => {
-          selector.sendKeys(text);
-        });
-      }
-    
-      public clickOnElement(selector: ElementFinder): void {
-        const ec = protractor.ExpectedConditions;
-        browser.wait(ec.elementToBeClickable(selector), 5000);
-        selector.click();
-      }
+  public enterTextInElement(selector: ElementFinder, text: any) {
+    const ec = protractor.ExpectedConditions;
+    browser.wait(ec.visibilityOf(selector));
+    selector.clear().then(() => {
+      selector.sendKeys(text);
+    });
   }
+
+  public clickOnElement(selector: ElementFinder): void {
+    const ec = protractor.ExpectedConditions;
+    browser.wait(ec.elementToBeClickable(selector), 5000);
+    selector.click();
+  }
+
+  public waitPageToLoad(url: string) {
+    const ec = protractor.ExpectedConditions;
+    return browser.wait(ec.urlContains(url), 5000);
+  }
+
+  public scrollDown() {
+    browser.executeScript('window.scrollTo(0,document.body.scrollHeight)');
+    browser.sleep(3000);
+  }
+
+  public numberOfRowsInTable(selector: ElementFinder) {
+    const numberOfRows: number = selector.count();
+    return numberOfRows;
+  }
+}
